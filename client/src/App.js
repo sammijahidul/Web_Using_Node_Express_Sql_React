7ada8b3df83a6407e3de2b261a9834221de60358
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2023-09-29 01:13:34 +0600

    created login, registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import CreatePost from './pages/CreatePost';
 import PostDetails from './pages/PostDetails';
+import LoginPage from './pages/LoginPage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
@@ -11,13 +13,16 @@ function App() {
         <div className='navbar'>
           <Link to="/">Home Page</Link>
           <Link to="/createpost">Create A Post</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/registrations">Registration</Link>
         </div>
         <Routes>
           <Route path='/' exact Component={HomePage}/>
           <Route path='/createpost' exact Component={CreatePost}/>
           <Route path='/post/:id' exact Component={PostDetails}/>
+          <Route path='/login' exact Component={LoginPage}/>
+          <Route path='/registrations' exact Component={RegistrationPage}/>
         </Routes>
-
       </Router>
     </div>
   );
---
+++ client/src/pages/RegistrationPage.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+// import {useNavigate} from 'react-router-dom'
+import * as Yup from 'yup'; 
+import axios from 'axios';
+
+
+function RegistrationPage() {
+//   let navigate = useNavigate();
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(8).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/api/v1/user/auth", data).then(() => {
+        console.log(data);
+    });
+  };
+
+  return (
+    <div>
+      <Formik 
+        initialValues={initialValues} 
+        onSubmit={onSubmit} 
+        validationSchema={validationSchema}
+      >
+        <Form className='formContainer'>
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field 
+            autoComplete="off" 
+            id="inputCreatePost" 
+            name="username" 
+            placeholder="(Ex: Jahid...)"
+          />
+
+          <label>Password: </label>
+          <ErrorMessage name="password" component="span" />
+          <Field 
+            autoComplete="off" 
+            type="password"
+            id="inputCreatePost" 
+            name="password" 
+            placeholder="(Ex: username@example.com)"
+          />
+
+          <button type='submit'>Register</button>
+        </Form>
+      </Formik>    
+    </div>
+  )
+}
+
+export default RegistrationPage
